2d816cd5e04da9c8e543a585fe4930141f65cecb
aryanbhojani <bhojaniaryan@gmail.com>
2025-02-02 13:07:55 -0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function Home() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/books") // Replace with your backend API URL
+      .then(response => setBooks(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  return (
+    <div className="home-container">
+      <h1>Kaede - Banned & Censored Books</h1>
+      <div className="book-list">
+        {books.map(book => (
+          <Link key={book.id} to={`/book/${book.id}`} className="book-item">
+            <h2>{book.title}</h2>
+            <p>{book.author}</p>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
 
