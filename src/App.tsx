29dac1a0342cc74bc1f88eed889423148c84530a
No767 <73260931+No767@users.noreply.github.com>
2025-02-02 10:44:47 -0800

    Revert "Add starter code"
    This reverts commit a77d0266498d2df9265bec4459f1b0f44be61c28.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import type React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import BookDetails from "./components/BookDetails"; // Add this after creating BookDetails component
-import BookList from "./components/BookList";
-
-const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<BookList />} />
-        <Route path="/book/:id" element={<BookDetails />} />{" "}
-        {/* Dynamic routing */}
-      </Routes>
-    </Router>
-  );
-};
+function App() {
+  return <></>;
+}
 
 export default App;
--- src/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@mantine/core";
-
-function MyComponent() {
-  return <Button color="blue">Click me</Button>;
-}
-
-export default MyComponent;
