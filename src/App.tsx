ab68beae5eefd2605a2de1e1fe3ae86a2479851b
aryanbhojani <bhojaniaryan@gmail.com>
2025-02-02 13:04:28 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Upload from "./pages/Upload";
+import BookDetails from "./pages/BookDetails";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
